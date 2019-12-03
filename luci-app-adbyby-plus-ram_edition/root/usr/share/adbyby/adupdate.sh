3e34533260e8c2ff97686238bfc08aa665f6da5a
Small_5 <Small_5>
2019-12-03 10:41:24 +0800

    Update Version

--- luci-app-adbyby-plus-ram_edition/root/usr/share/adbyby/adupdate.sh
+++ luci-app-adbyby-plus-ram_edition/root/usr/share/adbyby/adupdate.sh
@@ -1,108 +1,58 @@
 #!/bin/sh
-Status_1=$1
-
-init(){
+A=$1
+B=0
 	# 防止重复启动
-	local pid
 	for pid in $(pidof "${0##*/}"); do
-		[ "$pid" != "$$" ] && return 1
+		[ "$pid" != "$$" ] && exit 1
 	done
 
-	if ! mount | grep adbyby >/dev/null 2>&1; then
+	if ! mount | grep adbyby >/dev/null 2>&1;then
 		echo "Adbyby is not mounted,Stop update!"
-		return 1
+		exit 1
 	fi
 
-	while [ "$Status_1" == "check" ]; do
-		wget-ssl -4 --spider -q -t 1 -T 3 dev.tencent.com
-		[ "$?" != "0" ] && sleep 2 || break
-	done
-}
-
-download(){
-	rm -f /usr/share/adbyby/data/*.bak
-	rm -f /tmp/lazy.txt /tmp/video.txt
-
-	wget-ssl -4 -t 9 -T 3 -O /tmp/lazy.txt https://dev.tencent.com/u/adbyby/p/xwhyc-rules/git/raw/master/lazy.txt
-	if [ "$?" == "0" ]; then
-		echo "Download Adbyby general rules from dev.tencent.com success!"
-	else
-		echo "Download Adbyby general rules from Github..."
-		wget-ssl -4 -t 9 -T 10 -O /tmp/lazy.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt
-		if [ "$?" == "0" ]; then
-			echo "Download general rules success!"
-		else
-			echo "Download general rules failed!"
-			Status_Failed=1
-		fi
-	fi
-	wget-ssl -4 -t 9 -T 3 -O /tmp/video.txt https://dev.tencent.com/u/adbyby/p/xwhyc-rules/git/raw/master/video.txt
-	if [ "$?" == "0" ]; then
-		echo "Download Adbyby video rules from dev.tencent.com success!"
-	else
-		echo "Download Adbyby video rules from Github..."
-		wget-ssl -4 -t 9 -T 10 -O /tmp/video.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt
-		if [ "$?" == "0" ]; then
-			echo "Download video rules success!"
-		else
-			echo "Download video rules failed!"
-			Status_Failed=1
-		fi
-	fi
-	[ "$Status_Failed" == "1" ] && return 1
-}
-
-checkfile(){
 	md5sum /usr/share/adbyby/data/lazy.txt /usr/share/adbyby/data/video.txt > /tmp/local-md5.json
-	wget-ssl -4 -t 9 -T 3 -O /tmp/md5.json https://dev.tencent.com/u/adbyby/p/xwhyc-rules/git/raw/master/md5.json || wget-ssl -4 -t 9 -T 10 -O /tmp/md5.json https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/md5.json
-	[ ! -s "/tmp/md5.json" ] && return 1
-	lazy_local=$(grep 'lazy' /tmp/local-md5.json | awk -F' ' '{print $1}')
-	video_local=$(grep 'video' /tmp/local-md5.json | awk -F' ' '{print $1}')  
-	lazy_online=$(sed  's/":"/\n/g' /tmp/md5.json  |  sed  's/","/\n/g' | sed -n '2p')
-	video_online=$(sed  's/":"/\n/g' /tmp/md5.json  |  sed  's/","/\n/g' | sed -n '4p')
-	if [ "$lazy_online"x != "$lazy_local"x -o "$video_online"x != "$video_local"x ]; then
-		download
-		md5sum /tmp/lazy.txt /tmp/video.txt > /tmp/local-md5.json
+	wget-ssl -O /tmp/md5.json https://cdn.jsdelivr.net/gh/adbyby/xwhyc-rules/md5.json || wget-ssl -O /tmp/md5.json https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/md5.json
+	if [ -s "/tmp/md5.json" ];then
 		lazy_local=$(grep 'lazy' /tmp/local-md5.json | awk -F' ' '{print $1}')
-		video_local=$(grep 'video' /tmp/local-md5.json | awk -F' ' '{print $1}')
-		if [ "$lazy_online"x == "$lazy_local"x -a "$video_online"x == "$video_local"x ]; then
-			mv -f /tmp/lazy.txt /usr/share/adbyby/data/lazy.txt
-			mv -f /tmp/video.txt /usr/share/adbyby/data/video.txt
+		video_local=$(grep 'video' /tmp/local-md5.json | awk -F' ' '{print $1}')  
+		lazy_online=$(sed  's/":"/\n/g' /tmp/md5.json  |  sed  's/","/\n/g' | sed -n '2p')
+		video_online=$(sed  's/":"/\n/g' /tmp/md5.json  |  sed  's/","/\n/g' | sed -n '4p')
+		if [ "$lazy_online"x != "$lazy_local"x ];then
+			rm -f /usr/share/adbyby/data/*.bak /tmp/lazy.txt
+			wget-ssl -O /tmp/lazy.txt https://cdn.jsdelivr.net/gh/adbyby/xwhyc-rules/lazy.txt || wget-ssl -O /tmp/lazy.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt
+			cp -f /tmp/lazy.txt /usr/share/adbyby/data/lazy.txt
+			rm -f /tmp/lazy.txt
 			echo $(date +"%Y-%m-%d %H:%M:%S") > /tmp/adbyby.updated
-			Status_2=1
+			B=1
+		fi
+		if [ "$video_online"x != "$video_local"x ];then
+			rm -f /usr/share/adbyby/data/*.bak /tmp/video.txt
+			wget-ssl -O /tmp/video.txt https://cdn.jsdelivr.net/gh/adbyby/xwhyc-rules/video.txt || wget-ssl -O /tmp/video.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt
+			cp -f /tmp/video.txt /usr/share/adbyby/data/video.txt
+			rm -f /tmp/video.txt
+			echo $(date +"%Y-%m-%d %H:%M:%S") > /tmp/adbyby.updated
+			B=1
 		fi
 	fi
 	rm -f /tmp/local-md5.json /tmp/md5.json
-}
 
-judgment(){
-	if [ "$Status_1" == "restartdnsmasq" -a "$Status_2" == "1" ];then
+	if [ "$A" == "restartdnsmasq" ] && [ $B == 1 ];then
 		echo "Adbyby rules and Adblock rules need update!"
-		/etc/init.d/adbyby up_stop
-	elif [ "$Status_1" == "restartdnsmasq" -a "$Status_2" != "1" ];then
+		/etc/init.d/adbyby restart
+	elif [ "$A" == "restartdnsmasq" ] && [ $B != 1 ];then
 		echo "Adbyby rules no change!"
 		echo "Adblock rules need update!"
 		cp -f /tmp/adbyby/adbyby_adblock/dnsmasq.adblock /var/etc/dnsmasq-adbyby.d/04-dnsmasq.adblock
 		echo "Restart Dnsmasq"
 		/etc/init.d/dnsmasq restart >/dev/null 2>&1
-		rm -f /tmp/lazy.txt /tmp/video.txt
 		echo $(date +"%Y-%m-%d %H:%M:%S") > /tmp/adbyby.updated
-		return 0
-	elif [ "$Status_1" != "restartdnsmasq" -a "$Status_2" == "1" ];then
+	elif [ "$A" != "restartdnsmasq" ] && [ $B == 1 ];then
 		echo "Adbyby rules need update!"
-		/etc/init.d/adbyby up_stop
+		echo "Adblock rules no change!"
+		killall -q -9 adbyby
+		/usr/share/adbyby/adbyby &>/dev/null &
 	else
 		echo "All rules no change!"
-		rm -f /tmp/lazy.txt /tmp/video.txt
 		echo $(date +"%Y-%m-%d %H:%M:%S") > /tmp/adbyby.updated
-		return 0
 	fi
-	/etc/init.d/adbyby start
-}
-
-init
-checkfile || {
-	/etc/init.d/adbyby restart >/dev/null 2>&1 &
-	return 1
-}
-judgment
