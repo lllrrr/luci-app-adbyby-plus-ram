ec125fc6eb62f8be11a54e02bf2d2e7c2474ea46
Small_5 <Small_5>
2019-12-03 10:38:04 +0800

    Update Version

--- luci-app-adbyby-plus-ram_edition/root/usr/share/adbyby/adblock.sh
+++ luci-app-adbyby-plus-ram_edition/root/usr/share/adbyby/adblock.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
+	# 防止重复启动
+	for pid in $(ps -w | grep "${0##*/}" | grep -v grep | awk '{print $1}' &); do
+		[ "$pid" != "$$" ] && exit 1
+	done
 
-if ! mount | grep adbyby >/dev/null 2>&1; then
-	echo "Adbyby is not mounted,Stop update!"
-	return 1
-fi
-	wan_mode=`uci get adbyby.@adbyby[0].wan_mode 2>/dev/null`
-	if [ $wan_mode -eq 1 ]; then
+	if ! mount | grep adbyby >/dev/null 2>&1;then
+		echo "Adbyby is not mounted,Stop update!"
+		exit 1
+	fi
+
+	if [ "$(uci -q get adbyby.@adbyby[0].wan_mode)" == "1" ];then
 		rm -f /tmp/dnsmasq.adblock
-		wget -4 -t 9 -T 3 -O- https://dev.tencent.com/u/Small_5/p/adbyby/git/raw/master/easylistchina%2Beasylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > /tmp/dnsmasq.adblock || \
-		wget -4 -O- https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > /tmp/dnsmasq.adblock
+		wget -O- https://cdn.jsdelivr.net/gh/small-5/adbyby/easylistchina%2Beasylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > /tmp/dnsmasq.adblock || \
+		wget -O- https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > /tmp/dnsmasq.adblock
 		if [ -s "/tmp/dnsmasq.adblock" ];then
 			sed -i '/youku.com/d' /tmp/dnsmasq.adblock
 			if ( ! cmp -s /tmp/dnsmasq.adblock /tmp/adbyby/adbyby_adblock/dnsmasq.adblock );then
-				mv -f /tmp/dnsmasq.adblock /tmp/adbyby/adbyby_adblock/dnsmasq.adblock
+				cp -f /tmp/dnsmasq.adblock /tmp/adbyby/adbyby_adblock/dnsmasq.adblock
 				/usr/share/adbyby/adupdate.sh restartdnsmasq
 			else
-				rm -f /tmp/dnsmasq.adblock
 				/usr/share/adbyby/adupdate.sh
 			fi
+			rm -f /tmp/dnsmasq.adblock
 		fi
 	else
 		/usr/share/adbyby/adupdate.sh
